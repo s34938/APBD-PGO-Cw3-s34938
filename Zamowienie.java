1b8a5b58ce2ea2eb8e86fd2bd375169da5ce060a
s34938 <s34938@pjwstk.edu.pl>
2026-04-14 18:17:08 +0200

    Dodanie klasy Zamowwienie

---
+++ Zamowienie.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Zamowienie {
+    private int numerZamowienia;
+    private KlientKawiarni klient;
+    private ArrayList<ProduktMenu> produkty;
+    private boolean oplacone;
+    private static int kolejnyNumer = 1;
+
+    public Zamowienie(KlientKawiarni klient)
+    {
+        this.klient = klient;
+        this.numerZamowienia=kolejnyNumer++;
+        this.produkty = new ArrayList<>();
+        this.oplacone = false;
+    }
+}
