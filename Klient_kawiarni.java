87ed4819cac1eba8bcd04e7502cfcf711116433f
s34938 <s34938@pjwstk.edu.pl>
2026-04-14 18:00:02 +0200

    Dodanie klasy Klient kawiarni

---
+++ Klient_kawiarni.java
@@ -0,0 +1,17 @@
+import java.util.Objects;
+
+public class KlientKawiarni {
+    private int idKlienta;
+    private String imie;
+    private String nazwisko;
+    private String email;
+
+    public KlientKawiarni(int idKlienta, String imie, String nazwisko, String email)
+    {
+       this.idKlienta = idKlienta;
+       this.imie = imie;
+       this.nazwisko = nazwisko;
+       this.email = email;
+    }
+
+}
