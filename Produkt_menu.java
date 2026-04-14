bb208556a766d9c15ad0156ef39f6b47d90e9863
s34938 <s34938@pjwstk.edu.pl>
2026-04-14 17:29:11 +0200

    Stworzenie ProduktMenu i dodanie konstruktora

---
+++ Produkt_menu.java
@@ -0,0 +1,15 @@
+public class ProduktMenu {
+    private String kod;
+    private String nazwa;
+    private double cena;
+    private String kategoria;
+    private static int liczbaProduktow = 0;
+
+    public ProduktMenu(String kod, String nazwa, double cena, String kategoria){
+        this.kod = kod;
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.kategoria = kategoria;
+        liczbaProduktow;
+    }
+}
